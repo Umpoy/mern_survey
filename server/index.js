35f70cd46d523112f94da1f3238691fe9d353733
Umpoy <iumpoy@yahoo.com>
2018-12-19 12:32:40 -0800

    ejs renders to DOM

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send({ hello: "world" })
+    res.render('index');
 })
 
 app.listen(port, () => {
